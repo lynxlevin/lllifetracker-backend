7d326449055e36b6b10c2e1ab397637d92e27a22
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-07-22 09:50:53 +0900

    Code to run server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use llwinecellar_actix_backend::{settings as backend_settings, startup, telemetry};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenvy::dotenv().ok();
+
+    let settings = backend_settings::get_settings().expect("Failed to read settings.");
+
+    let subscriber = telemetry::get_subscriber(settings.clone().debug);
+    telemetry::init_subscriber(subscriber);
+
+    let application = startup::Application::build(settings).await?;
+
+    tracing::event!(target: "backend", tracing::Level::INFO, "Listening on http:/127.0.0.1:{}/", application.port());
+
+    application.run_until_stopped().await?;
+    Ok(())
 }
