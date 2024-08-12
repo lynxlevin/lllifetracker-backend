b86e1c763ee3d555e07c2a730f9800c8bd0e2f86
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-08-12 13:22:42 +0900

    Verification email

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,10 @@ pub mod startup;
 pub mod telemetry;
 pub mod types;
 pub mod utils;
+
+pub static ENV: once_cell::sync::Lazy<minijinja::Environment<'static>> =
+    once_cell::sync::Lazy::new(|| {
+        let mut env = minijinja::Environment::new();
+        env.set_loader(minijinja::path_loader("templates"));
+        env
+    });
