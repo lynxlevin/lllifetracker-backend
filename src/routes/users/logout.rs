cbd0af1f6875d18aec29b63879a7c1974f68d975
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-08-17 21:19:40 +0900

    Logout endpoint

---
+++ src/routes/users/logout.rs
@@ -0,0 +1,33 @@
+use actix_web::{post, HttpResponse};
+
+#[tracing::instrument(name = "Log out user", skip(session))]
+#[post("/logout/")]
+pub async fn log_out(session: actix_session::Session) -> HttpResponse {
+    match session_user_id(&session).await {
+        Ok(_) => {
+            tracing::event!(target: "backend", tracing::Level::INFO, "User retrieved from the DB.");
+            session.purge();
+            HttpResponse::Ok().json(crate::types::SuccessResponse {
+                message: "You have successfully logged out".to_string(),
+            })
+        }
+        Err(e) => {
+            tracing::event!(target: "backend", tracing::Level::ERROR, "Failed to get user from session: {:#?}", e);
+            HttpResponse::BadRequest().json(crate::types::ErrorResponse {
+                error:
+                    "We currently have some issues. Kindly try again and ensure you are logged in."
+                        .to_string(),
+            })
+        }
+    }
+}
+
+async fn session_user_id(session: &actix_session::Session) -> Result<uuid::Uuid, String> {
+    match session.get(crate::types::USER_ID_KEY) {
+        Ok(user_id) => match user_id {
+            None => Err("You are not authenticated".to_string()),
+            Some(id) => Ok(id),
+        },
+        Err(e) => Err(format!("{e}")),
+    }
+}
--- src/routes/users/mod.rs
+++ src/routes/users/mod.rs
@@ -1,5 +1,6 @@
 mod confirm_registration;
 mod login;
+mod logout;
 mod register;
 
 pub fn auth_routes_config(cfg: &mut actix_web::web::ServiceConfig) {
@@ -7,6 +8,7 @@ pub fn auth_routes_config(cfg: &mut actix_web::web::ServiceConfig) {
         actix_web::web::scope("/users")
             .service(register::register_user)
             .service(confirm_registration::confirm)
-            .service(login::login_user),
+            .service(login::login_user)
+            .service(logout::log_out),
     );
 }
