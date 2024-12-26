ff1ea4952b3c5981fc8fd6b0f5e96970bd919929
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-12-26 14:00:25 +0900

    Migration: Add archived to Ambition/Objective/Action

---
+++ migration/src/m20241226_000001_add_archived_to_ambition_objective_action_tables.rs
@@ -0,0 +1,81 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Ambition::Table)
+                    .add_column_if_not_exists(boolean(Ambition::Archived).default(false))
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Objective::Table)
+                    .add_column_if_not_exists(boolean(Objective::Archived).default(false))
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Action::Table)
+                    .add_column_if_not_exists(boolean(Action::Archived).default(false))
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Action::Table)
+                    .drop_column(Action::Archived)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Objective::Table)
+                    .drop_column(Objective::Archived)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Ambition::Table)
+                    .drop_column(Objective::Archived)
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+pub enum Ambition {
+    Table,
+    Archived,
+}
+
+#[derive(DeriveIden)]
+pub enum Objective {
+    Table,
+    Archived,
+}
+
+#[derive(DeriveIden)]
+pub enum Action {
+    Table,
+    Archived,
+}
