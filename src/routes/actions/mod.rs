fc91d9b80dfb55e80d419fd56b8ec96f458b7d7e
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-02-19 10:40:33 +0900

    bulk_update_ordering should take precedence to update

--- src/routes/actions/mod.rs
+++ src/routes/actions/mod.rs
@@ -14,10 +14,10 @@ pub fn action_routes(cfg: &mut ServiceConfig) {
             .service(list::list_actions)
             .service(get::get_action)
             .service(create::create_action)
+            .service(bulk_update_ordering::bulk_update_action_ordering)
             .service(update::update_action)
             .service(delete::delete_action)
-            .service(archive::archive_action)
-            .service(bulk_update_ordering::bulk_update_action_ordering),
+            .service(archive::archive_action),
         // MYMEMO: Can restrict AuthenticateUser this way.
         // .service(
         //     actix_web::web::scope("")
