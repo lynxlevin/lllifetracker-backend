7d4c5058e8f29e526f54a9dd368adc45d526d132
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-03-22 08:22:26 +0900

    feat (create_diary): validate score

--- src/routes/diaries/create.rs
+++ src/routes/diaries/create.rs
@@ -28,57 +28,69 @@ pub async fn create_diary(
     match user {
         Some(user) => {
             let user = user.into_inner();
-            match DiaryMutation::create(
-                &db,
-                NewDiary {
-                    text: req.text.clone(),
-                    date: req.date,
-                    score: req.score,
-                    tag_ids: req.tag_ids.clone(),
-                    user_id: user.id,
-                },
-            )
-            .await
-            {
-                Ok(diary) => {
-                    let res: DiaryVisible = diary.into();
-                    HttpResponse::Created().json(res)
-                }
-                Err(e) => {
-                    match &e {
-                        TransactionError::Transaction(e) => match e {
-                            DbErr::Query(SqlxError(Database(e))) => match e.constraint() {
-                                Some("diaries_user_id_date_unique_index") => {
-                                    return HttpResponse::Conflict().json(types::ErrorResponse {
-                                        error:
-                                            "Another diary record for the same date already exists."
-                                                .to_string(),
-                                    })
-                                }
-                                _ => {}
-                            },
-                            DbErr::Exec(SqlxError(Database(e))) => match e.constraint() {
-                                Some("fk-diaries_tags-tag_id") => {
-                                    return HttpResponse::NotFound().json(types::ErrorResponse {
-                                        error: "One or more of the tag_ids do not exist."
-                                            .to_string(),
-                                    })
-                                }
-                                _ => {}
-                            },
-                            _ => {}
+            match _validate_request_body(&req) {
+                Ok(_) => {
+                    match DiaryMutation::create(
+                        &db,
+                        NewDiary {
+                            text: req.text.clone(),
+                            date: req.date,
+                            score: req.score,
+                            tag_ids: req.tag_ids.clone(),
+                            user_id: user.id,
                         },
-                        _ => {}
+                    )
+                    .await
+                    {
+                        Ok(diary) => {
+                            let res: DiaryVisible = diary.into();
+                            HttpResponse::Created().json(res)
+                        }
+                        Err(e) => {
+                            match &e {
+                                TransactionError::Transaction(e) => match e {
+                                    DbErr::Query(SqlxError(Database(e))) => match e.constraint() {
+                                        Some("diaries_user_id_date_unique_index") => {
+                                            return HttpResponse::Conflict().json(types::ErrorResponse {
+                                                error:
+                                                    "Another diary record for the same date already exists."
+                                                        .to_string(),
+                                            })
+                                        }
+                                        _ => {}
+                                    },
+                                    DbErr::Exec(SqlxError(Database(e))) => match e.constraint() {
+                                        Some("fk-diaries_tags-tag_id") => {
+                                            return HttpResponse::NotFound().json(types::ErrorResponse {
+                                                error: "One or more of the tag_ids do not exist."
+                                                    .to_string(),
+                                            })
+                                        }
+                                        _ => {}
+                                    },
+                                    _ => {}
+                                },
+                                _ => {}
+                            }
+                            tracing::event!(target: "backend", tracing::Level::ERROR, "Failed on DB query: {:#?}", e);
+                            HttpResponse::InternalServerError().json(types::ErrorResponse {
+                                error: INTERNAL_SERVER_ERROR_MESSAGE.to_string(),
+                            })
+                        }
                     }
-                    tracing::event!(target: "backend", tracing::Level::ERROR, "Failed on DB query: {:#?}", e);
-                    HttpResponse::InternalServerError().json(types::ErrorResponse {
-                        error: INTERNAL_SERVER_ERROR_MESSAGE.to_string(),
-                    })
                 }
+                Err(e) => HttpResponse::BadRequest().json(types::ErrorResponse { error: e }),
             }
         }
         None => HttpResponse::Unauthorized().json("You are not logged in."),
     }
+}
+
+fn _validate_request_body(req: &RequestBody) -> Result<(), String> {
+    if req.score.is_some_and(|score| score > 5 || score < 1) {
+        return Err("score should be within 1 to 5.".to_string());
+    }
+    Ok(())
 }
 
 #[cfg(test)]
