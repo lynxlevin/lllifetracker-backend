0ffaf0f23c5371530ee1b53a36cc6819f3d64afa
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-03-22 08:26:30 +0900

    feat (update_diary): validate score

--- src/routes/diaries/update.rs
+++ src/routes/diaries/update.rs
@@ -35,65 +35,77 @@ pub async fn update_diary(
     match user {
         Some(user) => {
             let user = user.into_inner();
-            let form = UpdateDiary {
-                id: path_param.diary_id,
-                text: req.text.clone(),
-                date: req.date,
-                score: req.score,
-                tag_ids: req.tag_ids.clone(),
-                user_id: user.id,
-                update_keys: req.update_keys.clone(),
-            };
-            match DiaryMutation::partial_update(&db, form).await {
-                Ok(diary) => {
-                    let res: DiaryVisible = diary.into();
-                    HttpResponse::Ok().json(res)
-                }
-                Err(e) => {
-                    match &e {
-                        TransactionError::Transaction(e) => match e {
-                            DbErr::Query(SqlxError(Database(e))) => match e.constraint() {
-                                Some("diaries_user_id_date_unique_index") => {
-                                    return HttpResponse::Conflict().json(types::ErrorResponse {
-                                        error:
-                                            "Another diary record for the same date already exists."
-                                                .to_string(),
-                                    })
-                                }
+            match _validate_request_body(&req) {
+                Ok(_) => {
+                    let form = UpdateDiary {
+                        id: path_param.diary_id,
+                        text: req.text.clone(),
+                        date: req.date,
+                        score: req.score,
+                        tag_ids: req.tag_ids.clone(),
+                        user_id: user.id,
+                        update_keys: req.update_keys.clone(),
+                    };
+                    match DiaryMutation::partial_update(&db, form).await {
+                        Ok(diary) => {
+                            let res: DiaryVisible = diary.into();
+                            HttpResponse::Ok().json(res)
+                        }
+                        Err(e) => {
+                            match &e {
+                                TransactionError::Transaction(e) => match e {
+                                    DbErr::Query(SqlxError(Database(e))) => match e.constraint() {
+                                        Some("diaries_user_id_date_unique_index") => {
+                                            return HttpResponse::Conflict().json(types::ErrorResponse {
+                                                error:
+                                                    "Another diary record for the same date already exists."
+                                                        .to_string(),
+                                            })
+                                        }
+                                        _ => {}
+                                    },
+                                    DbErr::Exec(SqlxError(Database(e))) => match e.constraint() {
+                                        Some("fk-diaries_tags-tag_id") => {
+                                            return HttpResponse::NotFound().json(types::ErrorResponse {
+                                                error: "One or more of the tag_ids do not exist."
+                                                    .to_string(),
+                                            })
+                                        }
+                                        _ => {}
+                                    },
+                                    DbErr::Custom(e) => match e.parse::<CustomDbErr>().unwrap() {
+                                        CustomDbErr::NotFound => {
+                                            return HttpResponse::NotFound().json(types::ErrorResponse {
+                                                error: "Diary with this id was not found".to_string(),
+                                            })
+                                        }
+                                        _ => {}
+                                    },
+                                    _ => {}
+                                },
                                 _ => {}
-                            },
-                            DbErr::Exec(SqlxError(Database(e))) => match e.constraint() {
-                                Some("fk-diaries_tags-tag_id") => {
-                                    return HttpResponse::NotFound().json(types::ErrorResponse {
-                                        error: "One or more of the tag_ids do not exist."
-                                            .to_string(),
-                                    })
-                                }
-                                _ => {}
-                            },
-                            DbErr::Custom(e) => match e.parse::<CustomDbErr>().unwrap() {
-                                CustomDbErr::NotFound => {
-                                    return HttpResponse::NotFound().json(types::ErrorResponse {
-                                        error: "Diary with this id was not found".to_string(),
-                                    })
-                                }
-                                _ => {}
-                            },
-                            _ => {}
-                        },
-                        _ => {}
+                            }
+                            tracing::event!(target: "backend", tracing::Level::ERROR, "Failed on DB query: {:#?}", e);
+                            HttpResponse::InternalServerError().json(types::ErrorResponse {
+                                error: INTERNAL_SERVER_ERROR_MESSAGE.to_string(),
+                            })
+                        }
                     }
-                    tracing::event!(target: "backend", tracing::Level::ERROR, "Failed on DB query: {:#?}", e);
-                    HttpResponse::InternalServerError().json(types::ErrorResponse {
-                        error: INTERNAL_SERVER_ERROR_MESSAGE.to_string(),
-                    })
                 }
+                Err(e) => HttpResponse::BadRequest().json(types::ErrorResponse { error: e }),
             }
         }
         None => HttpResponse::Unauthorized().json(types::ErrorResponse {
             error: "You are not logged in".to_string(),
         }),
     }
+}
+
+fn _validate_request_body(req: &RequestBody) -> Result<(), String> {
+    if req.score.is_some_and(|score| score > 5 || score < 1) {
+        return Err("score should be within 1 to 5.".to_string());
+    }
+    Ok(())
 }
 
 #[cfg(test)]
@@ -274,6 +286,44 @@ mod tests {
 
         let res = test::call_service(&app, req).await;
         assert_eq!(res.status(), http::StatusCode::NOT_FOUND);
+
+        Ok(())
+    }
+
+    #[actix_web::test]
+    async fn validation_errors() -> Result<(), DbErr> {
+        let db = test_utils::init_db().await?;
+        let app = init_app(db.clone()).await;
+        let user = factory::user().insert(&db).await?;
+        let diary = factory::diary(user.id).insert(&db).await?;
+
+        let score_too_large_req = test::TestRequest::put()
+            .uri(&format!("/{}", diary.id))
+            .set_json(RequestBody {
+                text: None,
+                date: diary.date,
+                score: Some(6),
+                tag_ids: vec![],
+                update_keys: vec![DiaryKey::Score],
+            })
+            .to_request();
+        score_too_large_req.extensions_mut().insert(user.clone());
+        let res = test::call_service(&app, score_too_large_req).await;
+        assert_eq!(res.status(), http::StatusCode::BAD_REQUEST);
+
+        let score_too_small_req = test::TestRequest::put()
+            .uri(&format!("/{}", diary.id))
+            .set_json(RequestBody {
+                text: None,
+                date: diary.date,
+                score: Some(0),
+                tag_ids: vec![],
+                update_keys: vec![DiaryKey::Score],
+            })
+            .to_request();
+        score_too_small_req.extensions_mut().insert(user.clone());
+        let res = test::call_service(&app, score_too_small_req).await;
+        assert_eq!(res.status(), http::StatusCode::BAD_REQUEST);
 
         Ok(())
     }
