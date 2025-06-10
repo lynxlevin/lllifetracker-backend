0ad8e5ddced70dd602ba55a6c38e0c869fc5e0bd
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-06-10 16:02:05 +0900

    refactor: use login_attempt related settings in login endpoint

--- src/routes/users/login.rs
+++ src/routes/users/login.rs
@@ -5,6 +5,7 @@ use actix_web::{
     web::{Data, Json},
     HttpResponse,
 };
+use common::settings::types::Settings;
 use deadpool_redis::{
     redis::{AsyncCommands, SetExpiry, SetOptions},
     Connection, Pool,
@@ -21,72 +22,80 @@ pub struct LoginUser {
     password: String,
 }
 
-#[tracing::instrument(name = "Logging a user in", skip(db, redis_pool, req_user, session), fields(user_email = &req_user.email))]
+#[tracing::instrument(name = "Logging a user in", skip(db, redis_pool, req_user, session, settings), fields(user_email = &req_user.email))]
 #[post("/login")]
 async fn login_user(
     db: Data<DbConn>,
     redis_pool: Data<Pool>,
     req_user: Json<LoginUser>,
     session: actix_session::Session,
+    settings: Data<Settings>,
 ) -> HttpResponse {
     let not_found_message = "A user with these details does not exist. If you registered with these details, ensure you activate your account by clicking on the link sent to your e-mail address.";
     match redis_pool.get().await {
-        Ok(ref mut redis_con) => match validate_request_count(redis_con, &req_user.email).await {
-            Ok((login_request_count_key, login_request_count)) => {
-                match UserQuery::find_active_by_email(&db, req_user.email.clone()).await {
-                    Ok(user) => match user {
-                        Some(user) => {
-                            match verify_password(
-                                &user.password,
-                                req_user.password.clone().as_bytes(),
-                            ) {
-                                Ok(_) => {
-                                    tracing::event!(target: "backend", tracing::Level::INFO, "User logged in successfully.");
-                                    if let Err(e) = redis_con
-                                        .del::<String, String>(login_request_count_key)
-                                        .await
-                                    {
-                                        tracing::event!(target: "redis", tracing::Level::WARN, "Error deleting login_request_count_key from Redis: {:#?}", e)
-                                    };
-                                    match renew_session(session, user.id, user.email.clone()) {
-                                        Ok(_) => HttpResponse::Ok().json(::types::UserVisible {
-                                            id: user.id,
-                                            email: user.email,
-                                            first_name: user.first_name,
-                                            last_name: user.last_name,
-                                            is_active: user.is_active,
-                                        }),
-                                        Err(e) => response_500(e),
+        Ok(ref mut redis_con) => {
+            match validate_request_count(redis_con, &req_user.email, &settings).await {
+                Ok((login_request_count_key, login_request_count)) => {
+                    match UserQuery::find_active_by_email(&db, req_user.email.clone()).await {
+                        Ok(user) => match user {
+                            Some(user) => {
+                                match verify_password(
+                                    &user.password,
+                                    req_user.password.clone().as_bytes(),
+                                ) {
+                                    Ok(_) => {
+                                        tracing::event!(target: "backend", tracing::Level::INFO, "User logged in successfully.");
+                                        if let Err(e) = redis_con
+                                            .del::<String, String>(login_request_count_key)
+                                            .await
+                                        {
+                                            tracing::event!(target: "redis", tracing::Level::WARN, "Error deleting login_request_count_key from Redis: {:#?}", e)
+                                        };
+                                        match renew_session(session, user.id, user.email.clone()) {
+                                            Ok(_) => {
+                                                HttpResponse::Ok().json(::types::UserVisible {
+                                                    id: user.id,
+                                                    email: user.email,
+                                                    first_name: user.first_name,
+                                                    last_name: user.last_name,
+                                                    is_active: user.is_active,
+                                                })
+                                            }
+                                            Err(e) => response_500(e),
+                                        }
+                                    }
+                                    Err(_) => {
+                                        increment_login_request_count(
+                                            redis_con,
+                                            login_request_count_key,
+                                            login_request_count,
+                                            &settings,
+                                        )
+                                        .await;
+                                        response_404(not_found_message)
                                     }
                                 }
-                                Err(_) => {
-                                    increment_login_request_count(
-                                        redis_con,
-                                        login_request_count_key,
-                                        login_request_count,
-                                    )
-                                    .await;
-                                    response_404(not_found_message)
-                                }
                             }
-                        }
-                        None => {
-                            increment_login_request_count(
-                                redis_con,
-                                login_request_count_key,
-                                login_request_count,
-                            )
-                            .await;
-                            response_404(not_found_message)
-                        }
-                    },
-                    Err(e) => response_500(e),
+                            None => {
+                                increment_login_request_count(
+                                    redis_con,
+                                    login_request_count_key,
+                                    login_request_count,
+                                    &settings,
+                                )
+                                .await;
+                                response_404(not_found_message)
+                            }
+                        },
+                        Err(e) => response_500(e),
+                    }
                 }
+                Err(_) => HttpResponse::Unauthorized().json(::types::ErrorResponse {
+                    error: "Your account is temporarily locked. Please wait for 1 hour."
+                        .to_string(),
+                }),
             }
-            Err(_) => HttpResponse::Unauthorized().json(::types::ErrorResponse {
-                error: "Your account is temporarily locked. Please wait for 1 hour.".to_string(),
-            }),
-        },
+        }
         Err(e) => response_500(e),
     }
 }
@@ -94,13 +103,13 @@ async fn login_user(
 async fn validate_request_count(
     redis_con: &mut Connection,
     email: &str,
-) -> Result<(String, i32), String> {
-    let max_login_request_count = 5;
+    settings: &Settings,
+) -> Result<(String, u64), String> {
     let login_request_count_key = format!("login_count_{}", email);
     let login_request_count = redis_con.get(login_request_count_key.clone()).await.map_err(|e| {
         tracing::event!(target: "backend", tracing::Level::WARN, "Error getting login_request_count, defaults to 0: {}", e);
     }).unwrap_or(0);
-    if login_request_count >= max_login_request_count {
+    if login_request_count >= settings.application.max_login_attempts {
         Err("Too many login requests".to_string())
     } else {
         Ok((login_request_count_key, login_request_count))
@@ -113,23 +122,24 @@ fn renew_session(
     email: String,
 ) -> Result<(), SessionInsertError> {
     session.renew();
-    // MYMEMO: maybe ? does the same as this if let Err(e)
-    if let Err(e) = session.insert(USER_ID_KEY, id) {
-        return Err(e);
-    }
-    session.insert(USER_EMAIL_KEY, email)
+    session.insert(USER_ID_KEY, id)?;
+    session.insert(USER_EMAIL_KEY, email)?;
+    Ok(())
 }
 
 async fn increment_login_request_count(
     redis_con: &mut Connection,
     login_request_count_key: String,
-    login_request_count: i32,
+    login_request_count: u64,
+    settings: &Settings,
 ) -> () {
     if let Err(e) = redis_con
-        .set_options::<String, i32, String>(
+        .set_options::<String, u64, String>(
             login_request_count_key,
             login_request_count + 1,
-            SetOptions::default().with_expiration(SetExpiry::EX(3600)),
+            SetOptions::default().with_expiration(SetExpiry::EX(
+                settings.application.login_attempts_cool_time_seconds,
+            )),
         )
         .await
     {
