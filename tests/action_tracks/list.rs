6c76b4c43076d33177535b0dfb2aea2ed071d1cb
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-09-18 15:32:41 +0900

    feat: Add failing test

--- tests/action_tracks/list.rs
+++ tests/action_tracks/list.rs
@@ -1,6 +1,7 @@
 use actix_web::{http, test, HttpMessage};
-use chrono::{DateTime, Duration, FixedOffset, TimeDelta};
-use sea_orm::{ActiveModelTrait, DbErr};
+use chrono::{DateTime, Duration, FixedOffset, TimeDelta, Utc};
+use entities::action_track;
+use sea_orm::{ActiveModelTrait, ColumnTrait, DbErr, EntityTrait, QueryFilter};
 use use_cases::my_way::action_tracks::types::ActionTrackVisible;
 
 use crate::utils::Connections;
@@ -123,8 +124,57 @@ async fn happy_path_started_at_gte() -> Result<(), DbErr> {
 }
 
 #[actix_web::test]
+async fn happy_path_for_pagination() -> Result<(), DbErr> {
+    let Connections { app, db, .. } = init_app().await?;
+    let user = factory::user().insert(&db).await?;
+    let action = factory::action(user.id).insert(&db).await?;
+    let now = Utc::now();
+    let action_tracks = (1..26)
+        .map(|i| {
+            factory::action_track(user.id)
+                .started_at((now - TimeDelta::hours(i)).into())
+                .duration(Some(i))
+                .action_id(action.id)
+        })
+        .collect::<Vec<action_track::ActiveModel>>();
+    action_track::Entity::insert_many(action_tracks)
+        .exec(&db)
+        .await?;
+    let action_tracks = action_track::Entity::find()
+        .filter(action_track::Column::ActionId.eq(action.id))
+        .all(&db)
+        .await?;
+    let expected = &action_tracks[20..25];
+
+    let req = test::TestRequest::get()
+        .uri(&format!(
+            "/api/action_tracks?started_at_lte={}&batch_size={}",
+            expected[0].started_at.format("%Y-%m-%dT%H:%M:%SZ"),
+            10
+        ))
+        .to_request();
+    req.extensions_mut().insert(user.clone());
+
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), http::StatusCode::OK);
+
+    let res: Vec<ActionTrackVisible> = test::read_body_json(resp).await;
+
+    assert_eq!(res.len(), expected.len());
+    assert_eq!(
+        res,
+        expected
+            .iter()
+            .map(|track| ActionTrackVisible::from(track))
+            .collect::<Vec<_>>()
+    );
+
+    Ok(())
+}
+
+#[actix_web::test]
 async fn unauthorized_if_not_logged_in() -> Result<(), DbErr> {
-    let Connections { app, ..} = init_app().await?;
+    let Connections { app, .. } = init_app().await?;
 
     let req = test::TestRequest::get()
         .uri("/api/action_tracks")
