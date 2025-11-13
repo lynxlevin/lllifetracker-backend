fdc7b53fca9dc993e90a6edee72ee6e1117cf312
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-11-13 20:31:21 +0900

    feat (migration): remove trackable from action

---
+++ migration/src/m20251113_000001_remove_trackable_from_actions_table.rs
@@ -0,0 +1,44 @@
+use sea_orm_migration::{
+    prelude::{
+        async_trait,
+        sea_orm::{self, DeriveIden},
+        DbErr, DeriveMigrationName, MigrationTrait, SchemaManager, Table,
+    },
+    schema::boolean,
+};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Action::Table)
+                    .drop_column(Action::Trackable)
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Action::Table)
+                    .add_column_if_not_exists(boolean(Action::Trackable).default(true))
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+pub enum Action {
+    Table,
+    Trackable,
+}
