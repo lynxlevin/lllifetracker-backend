66fa07f17d200244916d9bb3d5c60930585087ea
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-12-30 21:40:27 +0900

    feat: use_case

---
+++ src/use_cases/journal/reading_notes/list_titles.rs
@@ -0,0 +1,21 @@
+use db_adapters::{
+    reading_note_adapter::{
+        ReadingNoteAdapter, ReadingNoteFilter, ReadingNoteOrder, ReadingNoteQuery,
+    },
+    Order::Asc,
+};
+use entities::user as user_entity;
+
+use crate::UseCaseError;
+
+pub async fn list_reading_note_titles<'a>(
+    user: user_entity::Model,
+    reading_note_adapter: ReadingNoteAdapter<'a>,
+) -> Result<Vec<String>, UseCaseError> {
+    reading_note_adapter
+        .filter_eq_user(&user)
+        .order_by_title(Asc)
+        .get_all_only_titles()
+        .await
+        .map_err(|e| UseCaseError::InternalServerError(format!("{:?}", e)))
+}
