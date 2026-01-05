d94a002751205ba3ebc0a6a30e584992c1db5020
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-01-05 14:09:00 +0900

    chore: add fixme comment

--- src/use_cases/journal/reading_notes/list_titles.rs
+++ src/use_cases/journal/reading_notes/list_titles.rs
@@ -14,6 +14,7 @@ pub async fn list_reading_note_titles<'a>(
 ) -> Result<Vec<String>, UseCaseError> {
     reading_note_adapter
         .filter_eq_user(&user)
+        // FIXME: This should better be ordered by updated_at desc, but could this be done in a performant way?
         .order_by_title(Asc)
         .get_all_only_titles()
         .await
