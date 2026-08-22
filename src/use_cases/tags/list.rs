e6d55029f0307020961f196ce76a5118465c4554
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-08-22 21:10:22 +0900

    fix: list_tags ordering

--- src/use_cases/tags/list.rs
+++ src/use_cases/tags/list.rs
@@ -10,12 +10,11 @@ pub async fn list_tags<'a>(
     user: user_entity::Model,
     tag_adapter: TagAdapter<'a>,
 ) -> Result<Vec<TagVisible>, UseCaseError> {
-    tag_adapter
+    let mut tags = tag_adapter
         .join_ambition()
         .join_direction()
         .join_action()
         .filter_eq_user(&user)
-        .order_by_type(Asc)
         .order_by_ambition_ordering_nulls_first(Asc)
         .order_by_direction_ordering_nulls_first(Asc)
         .order_by_action_ordering_nulls_first(Asc)
@@ -23,5 +22,7 @@ pub async fn list_tags<'a>(
         .get_all_tags()
         .await
         .map(|tags| tags.into_iter().map(|tag| TagVisible::from(tag)).collect::<Vec<_>>())
-        .map_err(|e| UseCaseError::InternalServerError(format!("{:?}", e)))
+        .map_err(|e| UseCaseError::InternalServerError(format!("{:?}", e)))?;
+    tags.sort_by_key(|tag| tag.r#type);
+    Ok(tags)
 }
