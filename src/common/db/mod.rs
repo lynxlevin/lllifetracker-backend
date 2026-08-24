6ead4f242db07a2e05c1ee4c79baba1321368a67
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-08-24 15:16:32 +0900

    refactor: db initialization

--- src/common/db/mod.rs
+++ src/common/db/mod.rs
@@ -1,19 +1,49 @@
 mod encryptor;
 
-use sea_orm::{Database, DbConn};
+use sea_orm::{ConnectionTrait, Database, DbConn, DbErr};
 
 use crate::settings::types::Settings;
 
 pub use encryptor::{decode_and_decrypt, encrypt_and_encode};
 
-pub async fn init_db(settings: &Settings) -> DbConn {
-    let db = Database::connect(&settings.database.url)
-        .await
-        .expect("Failed to open DB connection.");
+#[derive(Clone)]
+pub struct Db {
+    pub db: DbConn,
+}
 
-    db.get_schema_registry("entities::*")
-        .sync(&db)
+pub async fn get_db_connection(settings: &Settings) -> Result<Db, DbErr> {
+    let db = Database::connect(&settings.database.url).await?;
+    Ok(Db { db })
+}
+
+async fn db_migration(db: &DbConn) -> Result<(), DbErr> {
+    db.get_schema_registry("entities::*").sync(db).await
+}
+
+pub async fn init_db(settings: &Settings) -> Result<Db, DbErr> {
+    let db = get_db_connection(&settings).await?;
+    db_migration(&db.db).await?;
+    Ok(db)
+}
+
+pub async fn init_test_db(settings: &Settings) -> () {
+    let db = get_db_connection(&settings).await.unwrap();
+
+    db.db
+        .execute_unprepared(
+            "DROP TABLE IF EXISTS
+            friendship,
+            note,
+            note_permission,
+            \"user\",
+            tag,
+            tags_notes,
+            CASCADE;
+        ",
+        )
         .await
-        .expect("Failed in DB migration.");
-    db
+        .unwrap();
+
+    db_migration(&db.db).await.unwrap();
+    ()
 }
